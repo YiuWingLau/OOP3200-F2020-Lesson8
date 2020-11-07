b962778fb982aeee9707364c17637635ff3b5984
Lau Nelson <smartsonlau1997@gmail.com>
2020-11-06 23:52:00 -0500

    Full ICE7

---
+++ src/ca/durhamcollege/Main.java
@@ -0,0 +1,17 @@
+/**
+ * @author Tom Tsiliopoulos, StudentID: 100704716
+ * @date November 5, 2020
+ * @version 0.0.1
+ */
+package ca.durhamcollege;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        Person person = new Person("Tom", 40);
+        person.saysHello();
+
+    }
+}
